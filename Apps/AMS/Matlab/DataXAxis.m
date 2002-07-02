2e7e02f11c9bb8f127d22066507b1f21ae7d8d1e
Lowel P. O'Mard <lowel.omard@gmail.com>
2002-07-02 11:23:24 +0000

    Change: New DataXAxis routine to extract the X-axis information from the 'info' structure.

---
+++ Apps/AMS/Matlab/DataXAxis.m
@@ -0,0 +1,10 @@
+% This function closes the figure.
+%
+% usage DataXAxis(<info>)
+%
+% info -     info structure from DSAM AIFF data
+
+function x = DataXAxis(info)
+
+x = (info.outputTimeOffset:info.dt:info.outputTimeOffset+info.dt*(info.length-1))';
+
