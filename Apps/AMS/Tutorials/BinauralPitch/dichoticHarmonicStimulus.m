56b152e37b39ec7ed875a9772ba917a1b55279cd
Lowel P. O'Mard <lowel.omard@gmail.com>
2003-01-24 12:11:42 +0000

    Changes: (Ray Meddis).

--- Apps/AMS/Tutorials/BinauralPitch/dichoticHarmonicStimulus.m
+++ Apps/AMS/Tutorials/BinauralPitch/dichoticHarmonicStimulus.m
@@ -1,11 +1,12 @@
 function AMSgain=dichoticHarmonicStimulus(signalFile,F0,numHarmonics, firstHarmonic, duration, dt)
-cd C:\allProgs\collaborators\Oxenham\binauralPitch
+% cd C:\allProgs\collaborators\Oxenham\binauralPitch
 
 % 0dB SPL is signal level per component in micropascals
 
 t=dt:dt:duration; t=t';
 
 leftStimulus=zeros(length(t),1);
+
 for harmNum=firstHarmonic:2:firstHarmonic+numHarmonics-1
    leftStimulus=leftStimulus+20*sin(2*pi*F0*harmNum*t);
 end
@@ -14,6 +15,7 @@ rightStimulus=zeros(length(t),1);
 for harmNum=firstHarmonic+1:2:firstHarmonic+numHarmonics
    rightStimulus=rightStimulus+20*sin(2*pi*F0*harmNum*t);
 end
+
 signal=[leftStimulus rightStimulus];
 
 normFactor=max(max(abs(signal)));
@@ -23,3 +25,7 @@ wavwrite(signal,1/dt,16,signalFile)
 %plot(t,signal);
 
 AMSgain=20*log10(normFactor);
+figure(1)
+plot(signal(:,1))
+figure(2)
+plot(signal(:,2))
