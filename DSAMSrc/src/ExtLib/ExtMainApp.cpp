066060f4ca2048b5f4fafd268afa7142329420d2
Lowel P. O'Mard <lowel.omard@gmail.com>
2003-12-12 14:13:25 +0000

    Changes: These code modules were added as part of the new server implementation.

---
+++ DSAMSrc/src/ExtLib/ExtMainApp.cpp
@@ -0,0 +1,49 @@
+/**********************
+ *
+ * File:		ExtMainApp.cpp
+ * Purpose:		This routine contains the main handling code for when the non-
+ *				GUI mode is used
+ * Comments:	
+ * Author:		L. P. O'Mard
+ * Created:		12 Dec 2003
+ * Updated:		
+ * Copyright:	(c) 2003 CNBH, University of Essex
+ *
+ **********************/
+ 
+#ifdef HAVE_CONFIG_H
+#	include "DSAMSetup.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef WX_PRECOMP
+#	include <wx/wx.h>
+#endif
+
+#include "GeCommon.h"
+#include "GeSignalData.h"
+#include "GeEarObject.h"
+#include "GeUniParMgr.h"
+#include "GeModuleMgr.h"
+#include "UtDatum.h"
+#include "UtSSSymbols.h"
+#include "UtSimScript.h"
+#include "UtAppInterface.h"
+
+/******************************************************************************/
+/****************************** Global variables ******************************/
+/******************************************************************************/
+/******************************************************************************/
+/****************************** Methods (Subroutines) *************************/
+/******************************************************************************/
+
+/******************************************************************************/
+/****************************** Callbacks *************************************/
+/******************************************************************************/
+
+/******************************************************************************/
+/****************************** Functions *************************************/
+/******************************************************************************/
+
---
+++ DSAMSrc/src/GrLib/GrIPCServer.cpp
@@ -0,0 +1,96 @@
+/**********************
+ *
+ * File:		GrIPCServer.h
+ * Purpose:		Inter-process communication server extension code module for
+ *				The GUI.
+ * Comments:	This is a subclass of the IPCServer class (ExtIPCServer code 
+ *				module.
+ * Author:		L. P. O'Mard
+ * Created:		03 Dec 2003
+ * Updated:		
+ * Copyright:	(c) 2003, University of Essex
+ *
+ **********************/
+
+#ifdef HAVE_CONFIG_H
+#	include "DSAMSetup.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef WX_PRECOMP
+#	include <wx/wx.h>
+#endif
+
+#include "GeCommon.h"
+#include "GeSignalData.h"
+#include "GeEarObject.h"
+#include "ExtIPCServer.h"
+#include "GrIPCServer.h"
+#include "GrSimMgr.h"
+
+/******************************************************************************/
+/****************************** Global variables ******************************/
+/******************************************************************************/
+
+/******************************************************************************/
+/****************************** Methods (Subroutines) *************************/
+/******************************************************************************/
+
+/****************************** Constructor ***********************************/
+
+GrIPCServer::GrIPCServer(const wxString& hostName, uShort theServicePort):
+  IPCServer(hostName, theServicePort)
+{
+
+}
+
+/****************************** Destructor ************************************/
+
+GrIPCServer::~GrIPCServer(void)
+{
+	
+}
+
+/******************************************************************************/
+/****************************** IPCServer Methods *****************************/
+/******************************************************************************/
+
+/****************************** LoadSimFile ***********************************/
+
+/*
+ * This routine loads the simulation file.  This is a virtual function and
+ * classes that call this routine should create their on instance of this
+ * this function.
+ */
+
+void
+GrIPCServer::LoadSimFile(const wxString& fileName)
+{
+	wxGetApp().CreateDocument(fileName);
+
+}
+
+/****************************** SetNotification *******************************/
+
+/*
+ * Sets up the notification for event handling.
+ */
+
+void
+GrIPCServer::SetNotification(wxEvtHandler *theHandler)
+{
+	GetServer()->SetEventHandler(*theHandler, IPCSERVER_APP_SERVER_ID);
+	GetServer()->SetNotify(wxSOCKET_CONNECTION_FLAG);
+	GetServer()->Notify(TRUE);
+
+}
+
+/******************************************************************************/
+/****************************** Callbacks *************************************/
+/******************************************************************************/
+
+/******************************************************************************/
+/****************************** Functions *************************************/
+/******************************************************************************/
