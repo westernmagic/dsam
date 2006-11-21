5c6cb87c0738a59fdbda5a94e3fc9501841b6d3c
Lowel P. O'Mard <lowel.omard@gmail.com>
2006-11-21 20:20:51 +0000

    Corrected build problems.  The ams.cpp source code needed a path.

--- Apps/AMS/autogen.sh
+++ Apps/AMS/autogen.sh
@@ -4,7 +4,7 @@
 # File:		autogen.sh
 # Purpose:	This script runs all of the commands necessary for configuring DSAM
 #			from the SVN repository.
-# Author:	L.P.O'Mard
+# Author:	L.P.O'Mard 
 # Created:	21 Nov 2006
 # Updated:	
 # Copyright:(c) 2006, Lowel P. O'Mard
