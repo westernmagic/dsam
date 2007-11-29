21ed9556d195c8129d3b0adc53aee10ef7fb0352
Lowel P. O'Mard <lowel.omard@gmail.com>
2007-11-29 10:27:27 +0000

    Tried to change this file to executable again.

--- Apps/AMS/autogen.sh
+++ Apps/AMS/autogen.sh
@@ -2,7 +2,7 @@
 ##################
 #
 # File:		autogen.sh
-# Purpose:	This script runs all of the commands necessary for configuring DSAM
+# Purpose:	This script runs all of the commands necessary for configuring AMS
 #			from the SVN repository.
 # Author:	L.P.O'Mard
 # Created:	21 Nov 2006
