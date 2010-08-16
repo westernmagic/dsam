e748104655080cd21b38c0cddc54484295e2e49b
Lowel P. O'Mard <lowel.omard@gmail.com>
2010-08-16 17:55:19 +0000

    Change:  Updated the configuration to allow optional java support and moved the different interface options to different directories under src.

--- Apps/RunDSAMSim/RunDSAMSim.m
+++
@@ -1,9 +0,0 @@
-% RunDSAMSim:
-% 
-% [data, info] = RunDSAMSim(<sim file>, [<parameter settings>, [<signal>,...
-%                           <signal info.>]])
-% 
-% <sim file>           Simulation file name (string).
-% <parameter settings> Parameter '<name> <value> ...' pairs (string).
-% <signal>             Data signal ([chan, samples] real matrix).
-% <signal info.>       Signal information (structure).
