32e52c85d6952888bd4c010576d2531ca300be73
Lowel P. O'Mard <lowel.omard@gmail.com>
2012-11-10 07:04:28 +0000

    Changes: The autoconf unix configuration has been updated so that elements are corretly installed. I have ad the info module to DSAM which provided version information. I have yet to finish the addtion of the interface path so that the module knows where to find the LibRunDSAMSimGen shared library.

---
+++ Apps/RunDSAMSim/CfgTools/get_header_path.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import os.path
+import subprocess
+import sys
+
+pathList = subprocess.check_output("find /usr/lib -name " + sys.argv[1] +
+  " -print", shell=True)
+filePath = pathList.partition("\n")[0]
+dirPath = os.path.dirname(filePath)
+
+print dirPath
--- Apps/RunDSAMSim/src/python/dsam/interface.py
+++ Apps/RunDSAMSim/src/python/dsam/interface.py
@@ -3,6 +3,7 @@ from ctypes import POINTER
 import os 
 import numpy as np
 import dsam
+import dsam.info
 
 class info:
     def __init__(self, sig):
@@ -16,17 +17,9 @@ class info:
         self.interleaveLevel = sig.interleaveLevel
         self.wordSize = 2.0
 
-interfacePathEnvVar = "DSAM_DLL_PATH"
 baseInterfaceName = "libRunDSAMSimGen"
-
 interfaceName = baseInterfaceName + ".so" if (os.name == "posix") else baseInterfaceName
-try:
-    interfacePath = os.environ[interfacePathEnvVar]
-except KeyError:
-    print "The '" + interfacePathEnvVar + "' environment variable must be set."
-    raise
-
-lib = np.ctypeslib.load_library(interfaceName, interfacePath)
+lib = np.ctypeslib.load_library(interfaceName, dsam.info.GetDirLib())
 
 libRunDSAMSim = lib.RunDSAMSim
 libRunDSAMSim.argtypes = [c_wchar_p, c_wchar_p, POINTER(dsam.SignalData)]
